4c6cd6203c51392cd0d406a4e263b2b8914ad283
Ben Garren <bngarren@gmail.com>
2022-06-23 06:44:38 -0400

    Add debug route, display recent regions/resources, update to client app to use this endpoint

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,8 +1,9 @@
 import express from "express";
-import { playerAction } from "../controllers";
+import { debug, playerAction } from "../controllers";
 
 const router = express.Router();
 
 router.post("/scan", playerAction.scan);
+router.get("/debug", debug.showRecent);
 
 export default router;
