2302b9911c1c94e10732ec402b037647de3992a5
Ben Garren <bngarren@gmail.com>
2022-06-23 20:51:55 -0400

    Add leaflet and react-leaflet to get client map working. Shows user pos and resources on map! Tests passing

--- src/types/scanService.types.ts
+++ src/types/scanService.types.ts
@@ -2,6 +2,8 @@ import type { RegionType } from "../models/Region";
 import { ResourceType } from "../models/Resource";
 
 export type ScanResultResource = ResourceType & {
+  resourcePosition: number[];
+  vertices: number[][];
   distanceFromUser: number;
   userCanInteract: boolean;
 };
